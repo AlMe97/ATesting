b29cfc7b4c78f2f073352949c6289c8baec68abf
Alina Misaila <ali_misaila@yahoo.com>
2020-09-09 01:51:49 +0300

    Added first automation testing code - python, pycharm

---
+++ testare1/test.py
@@ -0,0 +1,30 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+
+PATH = "C:\Program Files (x86)\chromedriver.exe"
+driver = webdriver.Chrome(PATH)
+
+driver.get("https://google.ro")
+#print(driver.title)
+
+time.sleep(2)
+
+searchBox = driver.find_element_by_css_selector("#tsf input.gLFyf.gsfi")
+searchBox.send_keys("world of warcraft")
+searchBox.send_keys(Keys.RETURN)
+
+time.sleep(2)
+
+# Method 1
+# results = driver.find_elements_by_xpath('//div[@class="r"]/a/h3')  # finds webresults
+# results[0].click() # clicks the first one
+
+# Method 2
+firstSite = driver.find_element_by_css_selector("#rso .g:nth-of-type(1) .rc div.r a h3")
+firstSite.click()
+
+time.sleep(2)
+
+#driver.close() #close current tab
+driver.quit() #close entire browser
