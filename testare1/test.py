4705d9b97594f28c3a9d861cc3adcc1e607bd811
Alina Misaila <ali_misaila@yahoo.com>
2020-09-09 01:58:02 +0300

    Added comments

--- testare1/test.py
+++ testare1/test.py
@@ -1,3 +1,6 @@
+# This code opens the google.ro webpage, types "World of Warcraft" in the search bar, then searches it on google.
+# Then, it clicks on the first result, so it opens the World of Warcraft main page. - 2 methods coded for this
+
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
@@ -6,7 +9,7 @@ PATH = "C:\Program Files (x86)\chromedriver.exe"
 driver = webdriver.Chrome(PATH)
 
 driver.get("https://google.ro")
-#print(driver.title)
+#print(driver.title) #prints out title of current webpage
 
 time.sleep(2)
 
@@ -17,8 +20,8 @@ searchBox.send_keys(Keys.RETURN)
 time.sleep(2)
 
 # Method 1
-# results = driver.find_elements_by_xpath('//div[@class="r"]/a/h3')  # finds webresults
-# results[0].click() # clicks the first one
+# results = driver.find_elements_by_xpath('//div[@class="r"]/a/h3')  #finds webresults
+# results[0].click() #clicks the first one
 
 # Method 2
 firstSite = driver.find_element_by_css_selector("#rso .g:nth-of-type(1) .rc div.r a h3")
